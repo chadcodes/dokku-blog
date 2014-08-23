d53f5e064bba46f2bf2abb8bdcc6e478b4b588cb
Chad <chadcodes@users.noreply.github.com>
2014-08-23 17:13:42 -0400

    add user sign in plus default url for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Actionmailer needs to know the default URL before it can send emails
+  #config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
